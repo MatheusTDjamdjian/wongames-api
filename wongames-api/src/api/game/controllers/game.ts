621c51eed08c5be79c65283b11bcb6eb08f71675
Matheus <matheustassodjamdjian@gmail.com>
2025-02-28 15:43:27 -0300

    creating simple route and controller

--- wongames-api/src/api/game/controllers/game.ts
+++ wongames-api/src/api/game/controllers/game.ts
@@ -1,7 +1,12 @@
-/**
- * game controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+    "api::game.game",
+    ({ strapi }) => ({
+      async populate(ctx) {
+        console.log("RODANDO NO SERVIDOR");
+  
+        ctx.send("FINALIZADO NO CLIENT");
+      },
+    })
+  );
