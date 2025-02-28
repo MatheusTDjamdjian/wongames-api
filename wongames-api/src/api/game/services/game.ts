1a4c69d72d8e7fc661de6f9c48235f0e91662501
Matheus <matheustassodjamdjian@gmail.com>
2025-02-28 16:41:34 -0300

    searching the gog api data

--- wongames-api/src/api/game/services/game.ts
+++ wongames-api/src/api/game/services/game.ts
@@ -1,11 +1,14 @@
+import axios from 'axios';
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::game.game", () => ({
     async populate(params) {
-      const cat = await strapi.service("api::category.category").find({
-        filters: { name: params.category },
-      });
-  
-      console.log(cat);
+        const gogApiUrl = `https:///catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
+
+        const {
+          data: { products },
+        } = await axios.get(gogApiUrl);
+
+        console.log(products[0]);
     },
   }));
