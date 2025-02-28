d320ae7bdd0c360264236783e32914e317320163
Matheus <matheustassodjamdjian@gmail.com>
2025-02-28 17:20:01 -0300

    scrapping data with jsdom

--- wongames-api/src/api/game/services/game.ts
+++ wongames-api/src/api/game/services/game.ts
@@ -1,14 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
+import { JSDOM } from "jsdom";
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::game.game", () => ({
-    async populate(params) {
-        const gogApiUrl = `https:///catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
+    async getGameInfo(slug) {
+    const gogSlug = slug.replaceAll('-', '_').toLowerCase();
 
-        const {
-          data: { products },
-        } = await axios.get(gogApiUrl);
+    const body = await axios.get(`https://www.gog.com/game/${gogSlug}`);
+    const dom = new JSDOM(body.data);
 
-        console.log(products[0]);
-    },
-  }));
+    const raw_description = dom.window.document.querySelector(".description");
+
+    const description = raw_description.innerHTML
+    const short_description = raw_description.textContent.slice(0,160);
+
+    const ratingElement = dom.window.document.querySelector(".age-restrictions__icon use");
+
+    return {
+        description,
+        short_description,
+        rating: ratingElement ? ratingElement
+        .getAttribute('xlink:href')
+        .replace(/_/g, " ")
+        .replace("#", " ") 
+        : "BR0",
+    }
+  },
+
+  async populate() {
+    const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(await this.getGameInfo(products[2].slug));
+  },
+}));
