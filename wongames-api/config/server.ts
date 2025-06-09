4b4d5b23e92531f0322298c2d2ebe8fa63cc65a4
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-06-09 14:51:04 -0300

    fx npm and api

--- wongames-api/config/server.ts
+++ wongames-api/config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  console.log('APP_KEYS array:', env.array('APP_KEYS'));
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
