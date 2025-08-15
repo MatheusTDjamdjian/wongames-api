f7ccabb409c74a4c32c7cee12aa80b609acea53d
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-08-15 17:55:03 -0300

    Add Banners content type

--- wongames-api/types/generated/components.d.ts
+++ wongames-api/types/generated/components.d.ts
@@ -1,3 +1,42 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PageButton extends Struct.ComponentSchema {
+  collectionName: 'components_page_buttons';
+  info: {
+    displayName: 'button';
+    icon: 'link';
+  };
+  attributes: {
+    label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Buy now'>;
+    link: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface PageRibbon extends Struct.ComponentSchema {
+  collectionName: 'components_page_ribbons';
+  info: {
+    displayName: 'ribbon';
+    icon: 'cog';
+  };
+  attributes: {
+    color: Schema.Attribute.Enumeration<['primary', 'secondary']> &
+      Schema.Attribute.DefaultTo<'primary'>;
+    size: Schema.Attribute.Enumeration<['small', 'normal']> &
+      Schema.Attribute.DefaultTo<'normal'>;
+    text: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 40;
+      }>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'page.button': PageButton;
+      'page.ribbon': PageRibbon;
+    }
+  }
+}
--- wongames-api/types/generated/contentTypes.d.ts
+++ wongames-api/types/generated/contentTypes.d.ts
@@ -373,6 +373,42 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiBannerBanner extends Struct.CollectionTypeSchema {
+  collectionName: 'banners';
+  info: {
+    displayName: 'banner';
+    pluralName: 'banners';
+    singularName: 'banner';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    button: Schema.Attribute.Component<'page.button', false>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::banner.banner'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    ribbon: Schema.Attribute.Component<'page.ribbon', false>;
+    subtitle: Schema.Attribute.Text &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 150;
+      }>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
   collectionName: 'categories';
   info: {
@@ -1067,6 +1103,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::banner.banner': ApiBannerBanner;
       'api::category.category': ApiCategoryCategory;
       'api::developer.developer': ApiDeveloperDeveloper;
       'api::game.game': ApiGameGame;
