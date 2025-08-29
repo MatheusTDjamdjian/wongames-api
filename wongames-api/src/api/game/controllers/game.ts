48d4da27c75a7ea0dbdc3d6be20e6ce945f6d086
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-08-29 14:33:27 -0300

    change populate

--- wongames-api/src/api/game/controllers/game.ts
+++ wongames-api/src/api/game/controllers/game.ts
@@ -8,11 +8,10 @@ export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
     async populate(ctx) {
-      const options = {
-        limit: 100,
-        order: "desc:trending",
-        ...ctx.query,
-      };
+      const limit = Number(ctx.query.limit) || 100;
+      const offset = Number(ctx.query.offset) || 0;
+
+      const options = { limit, offset };
 
       await strapi.service("api::game.game").populate(options);
 
